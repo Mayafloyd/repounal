50a29b357abd98c918eccecd8d5b4e7307da7c71
Sara Maya <saacevedom@unal.edu.co>
2024-10-09 12:01:26 -0500

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="p-8 flex justify-center items-center">
-    <div class="p-5 bg-purple-500 text-white rounded">hi</div>
+    <div class="p-5 bg-purple-500 text-white rounded">test</div>
 </div>
